57de30349787ba805da92d76423a0d66583dd78d
Harshit Gupta <mail4harshit1@gmail.com>
2020-11-11 03:25:42 +0530

    q2 finalised

--- code/q2/q2bc.m
+++ code/q2/q2bc.m
@@ -15,22 +15,26 @@ covariance_boxplot_matrix = zeros(100, length(ns));
 for k = 1:length(ns)
    for m = 1:100
        n = ns(k); % current n
-       standard_sample = randn(2, n); % vectorised sampling, sample is in a 2xN matrix
-       % where every column is a sample
-       sample = MU + A*standard_sample; % this gives a 2 x N matrix where every column is a sample
+       standard_sample = randn(2, n); 
+       % vectorised sampling, sample is in a 2xN matrix where every column 
+       % is a sample
+       sample = MU + A*standard_sample; 
+       % this gives a 2 x N matrix where every column is a sample
 
        mean_vector = [0 0]';
        for l=1:n
-          mean_vector = mean_vector + sample(:, l); % this can be done using sum
-          % but we have been instructed to use only eig and randn, with sum
-          % this is a single line
+          mean_vector = mean_vector + sample(:, l);
+          % this can be done using sum(.) but we have been instructed to 
+          % use only eig and randn, with sum this is a single line
        end
        mean_vector = mean_vector/n;
        
        error = norm(mean_vector - MU)/norm(MU);
-       mean_boxplot_matrix(m, k) = error; % current error value at the mth trial for n
+       mean_boxplot_matrix(m, k) = error;
+       % current error value at the mth trial for n
        
-       sample = sample - mean_vector; % subtracted mean from sample (to center at origin)
+       sample = sample - mean_vector; 
+       % subtracted mean from sample (to center at origin)
        current_covariance = sample*sample'/n;
        % in the above line we are using the vectorised implementation for 
        % getting sample covariance. sample is 2xN, sample' is Nx2.
